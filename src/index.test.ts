cc72ea4ab44fbce7aaf5fa9ae543a77371618532
Giulliano Bueno <giulliano.bueno@blacklane.com>
2020-05-14 09:42:03 +0200

    Add the logging code

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,57 @@
-import { sum } from '.'
+import logger from '.'
 
-describe('blah', () => {
-  it('works', () => {
-    expect.assertions(1)
-    expect(sum(1, 1)).toStrictEqual(2)
+describe('Logger', () => {
+  describe('info', () => {
+    it('generates a log with level info', () => {
+      expect.assertions(2)
+
+      const spy = jest.spyOn(console, 'info')
+      logger.info({ message: 'Nada' }, 'test', 'it')
+      const message = JSON.parse(spy.mock.calls[0][0])
+
+      expect(console.info).toHaveBeenCalledTimes(1)
+      expect(message.log_level).toStrictEqual('info')
+
+      spy.mockRestore()
+    })
+
+    it('generates a log with the body as the JSON passed', () => {
+      expect.assertions(2)
+
+      const spy = jest.spyOn(console, 'info')
+      logger.info({ message: 'Nada' }, 'test', 'it')
+      const message = JSON.parse(spy.mock.calls[0][0])
+
+      expect(console.info).toHaveBeenCalledTimes(1)
+      expect(message.body).toStrictEqual({ message: 'Nada' })
+
+      spy.mockRestore()
+    })
+
+    it('generates a log with level warn', () => {
+      expect.assertions(2)
+
+      const spy = jest.spyOn(console, 'warn')
+      logger.warn({ message: 'Nada' }, 'test', 'it')
+      const message = JSON.parse(spy.mock.calls[0][0])
+
+      expect(console.warn).toHaveBeenCalledTimes(1)
+      expect(message.log_level).toStrictEqual('warn')
+
+      spy.mockRestore()
+    })
+
+    it('generates a log with level error', () => {
+      expect.assertions(2)
+
+      const spy = jest.spyOn(console, 'error')
+      logger.error({ message: 'Nada' }, 'test', 'it')
+      const message = JSON.parse(spy.mock.calls[0][0])
+
+      expect(console.error).toHaveBeenCalledTimes(1)
+      expect(message.log_level).toStrictEqual('error')
+
+      spy.mockRestore()
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,60 @@
-export const sum = (a: number, b: number) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('boop')
-  }
-  return a + b
+enum LogLevel {
+  INFO = 'info',
+  WARN = 'warn',
+  ERROR = 'error',
 }
+
+/**
+ * Creates the message structure.
+ *
+ * @param message is a JSON object that represents the body of the event.
+ * @param event  is the name of request event (request_started, request_finished and etc). The default is request_finished.
+ * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
+ * @param logLevel is the log level (info, warn, error).
+ */
+function createMessage (message: unknown, event: string, messageEvent: string, logLevel: LogLevel): string {
+  return JSON.stringify({
+    timestamp: new Date().toISOString(),
+    log_level: logLevel,
+    event: event,
+    body: message,
+    message: {
+      event: messageEvent
+    }
+  })
+}
+
+/**
+ * Logs an info log.
+ *
+ * @param message is a JSON object that represents the body of the event.
+ * @param event  is the name of request event (request_started, request_finished and etc)
+ * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
+ */
+function info (message: unknown, event: string, messageEvent: string): void {
+  console.info(createMessage(message, event, messageEvent, LogLevel.INFO))
+}
+
+/**
+ * Logs a warn log.
+ *
+ * @param message is a JSON object that represents the body of the event.
+ * @param event  is the name of request event (request_started, request_finished and etc)
+ * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
+ */
+function warn (message: unknown, event: string, messageEvent: string): void {
+  console.warn(createMessage(message, event, messageEvent, LogLevel.WARN))
+}
+
+/**
+ * Logs an error log.
+ *
+ * @param message is a JSON object that represents the body of the event.
+ * @param event  is the name of request event (request_started, request_finished and etc)
+ * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
+ */
+function error (message: unknown, event: string, messageEvent: string): void {
+  console.error(createMessage(message, event, messageEvent, LogLevel.ERROR))
+}
+
+export default { info, warn, error }
