1735332aae57aa790815bc404231ed9f7dfe1d97
Rodrigo Pinto <rodrigopqn@gmail.com>
2020-05-18 17:27:42 +0200

    Redesign the Kiev lib to make it support more configurations.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,98 @@
-import logger from '.'
+import { Kiev } from '.'
 
-describe('logger', () => {
-  describe('info', () => {
-    it('generates a log with level info', () => {
-      expect.assertions(2)
+let Logger: Kiev
+let applicationName: string
+let environment: string
+
+const logPayload = {
+  user: {
+    first_name: 'John',
+    last_name: 'Doe'
+  }
+}
+let defaultLogAttributes: string[]
+
+describe('kiev', () => {
+  beforeAll(() => {
+    applicationName = 'application-name'
+    environment = 'development'
+    defaultLogAttributes = [
+      'application',
+      'environment',
+      'level',
+      'message',
+      'timestamp'
+    ]
+    Logger = new Kiev(applicationName, environment)
+  })
+
+  describe('logging different levels', () => {
+    it('outputs with DEBUG', () => {
+      expect.assertions(6)
+
+      const spy = jest.spyOn(console, 'debug')
+      Logger.debug('There were an event here, see the payload', logPayload)
+      const message = JSON.parse(spy.mock.calls[0][0])
+
+      expect(console.debug).toHaveBeenCalledTimes(1)
+      expect(Object.keys(message)).toStrictEqual(
+        expect.arrayContaining(defaultLogAttributes)
+      )
+      expect(message.application).toStrictEqual(applicationName)
+      expect(message.level).toStrictEqual('DEBUG')
+      expect(message.environment).toStrictEqual(environment)
+      expect(message).toMatchObject(logPayload)
+    })
+
+    it('outputs with INFO', () => {
+      expect.assertions(6)
 
       const spy = jest.spyOn(console, 'info')
-      logger.info({ message: 'Nada' }, 'test', 'it')
+      Logger.info('There were an event here, see the payload', logPayload)
       const message = JSON.parse(spy.mock.calls[0][0])
 
       expect(console.info).toHaveBeenCalledTimes(1)
-      expect(message.log_level).toStrictEqual('info')
-
-      spy.mockRestore()
+      expect(Object.keys(message)).toStrictEqual(
+        expect.arrayContaining(defaultLogAttributes)
+      )
+      expect(message.application).toStrictEqual(applicationName)
+      expect(message.level).toStrictEqual('INFO')
+      expect(message.environment).toStrictEqual(environment)
+      expect(message).toMatchObject(logPayload)
     })
 
-    it('generates a log with the body as the JSON passed', () => {
-      expect.assertions(2)
-
-      const spy = jest.spyOn(console, 'info')
-      logger.info({ message: 'Nada' }, 'test', 'it')
-      const message = JSON.parse(spy.mock.calls[0][0])
-
-      expect(console.info).toHaveBeenCalledTimes(1)
-      expect(message.body).toStrictEqual({ message: 'Nada' })
-
-      spy.mockRestore()
-    })
-
-    it('generates a log with level warn', () => {
-      expect.assertions(2)
+    it('outputs with WARN', () => {
+      expect.assertions(6)
 
       const spy = jest.spyOn(console, 'warn')
-      logger.warn({ message: 'Nada' }, 'test', 'it')
+      Logger.warn('There were an event here, see the payload', logPayload)
       const message = JSON.parse(spy.mock.calls[0][0])
 
       expect(console.warn).toHaveBeenCalledTimes(1)
-      expect(message.log_level).toStrictEqual('warn')
-
-      spy.mockRestore()
+      expect(Object.keys(message)).toStrictEqual(
+        expect.arrayContaining(defaultLogAttributes)
+      )
+      expect(message.application).toStrictEqual(applicationName)
+      expect(message.level).toStrictEqual('WARN')
+      expect(message.environment).toStrictEqual(environment)
+      expect(message).toMatchObject(logPayload)
     })
 
-    it('generates a log with level error', () => {
-      expect.assertions(2)
+    it('outputs with ERROR', () => {
+      expect.assertions(6)
 
       const spy = jest.spyOn(console, 'error')
-      logger.error({ message: 'Nada' }, 'test', 'it')
+      Logger.error('There were an event here, see the payload', logPayload)
       const message = JSON.parse(spy.mock.calls[0][0])
 
       expect(console.error).toHaveBeenCalledTimes(1)
-      expect(message.log_level).toStrictEqual('error')
-
-      spy.mockRestore()
+      expect(Object.keys(message)).toStrictEqual(
+        expect.arrayContaining(defaultLogAttributes)
+      )
+      expect(message.application).toStrictEqual(applicationName)
+      expect(message.level).toStrictEqual('ERROR')
+      expect(message.environment).toStrictEqual(environment)
+      expect(message).toMatchObject(logPayload)
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,91 @@
 enum LogLevel {
-  INFO = 'info',
-  WARN = 'warn',
-  ERROR = 'error',
+  DEBUG = 'DEBUG',
+  INFO = 'INFO',
+  WARN = 'WARN',
+  ERROR = 'ERROR'
 }
 
-interface Message {
+interface LogMessage {
+  application: string
+  environment: string
+  level: LogLevel
+  message: string
   timestamp: string
-  log_level: string
-  event: string
-  body: unknown
-  message: {
-    event: string
-  }
 }
 
-/**
- * Creates the message structure.
- *
- * @param message is a JSON object that represents the body of the event.
- * @param event  is the name of request event (request_started, request_finished and etc). The default is request_finished.
- * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
- * @param logLevel is the log level (info, warn, error).
- */
-// prettier-ignore
-function createMessage (
-  message: unknown,
-  event: string,
-  messageEvent: string,
-  logLevel: LogLevel
-): string | Message {
-  const data: Message = {
-    timestamp: new Date().toISOString(),
-    log_level: logLevel,
-    event: event,
-    body: message,
-    message: {
-      event: messageEvent
+class Kiev {
+  application: string
+  environment: string
+  level: LogLevel
+
+  constructor (application: string, environment: string) {
+    this.application = application
+    this.environment = environment
+    this.level = LogLevel.INFO
+  }
+
+  /**
+   * Generates an output log with severity level INFO.
+   *
+   * @param message  [String] is the name of log event(i.e. request_started, request_finished and etc).
+   * @param payload [Object] is a JSON object that represents the content of the event.
+   */
+  info (message: string, payload: Object): void {
+    console.info(this._buildPayload(LogLevel.INFO, message, payload))
+  }
+
+  /**
+   * Generates an output log with severity level DEBUG.
+   *
+   * @param message  [String] is the name of log event(i.e. request_started, request_finished and etc).
+   * @param payload [Object] is a JSON object that represents the content of the event.
+   */
+  debug (message: string, payload: Object): void {
+    console.debug(this._buildPayload(LogLevel.DEBUG, message, payload))
+  }
+
+  /**
+   * Generates an output log with severity level ERROR.
+   *
+   * @param message  [String] is the name of log event(i.e. request_started, request_finished and etc).
+   * @param payload [Object] is a JSON object that represents the content of the event.
+   */
+  error (message: string, payload: Object): void {
+    console.error(this._buildPayload(LogLevel.ERROR, message, payload))
+  }
+
+  /**
+   * Generates an output log with severity level WARN.
+   *
+   * @param message  [String] is the name of log event(i.e. request_started, request_finished and etc).
+   * @param payload [Object] is a JSON object that represents the content of the event.
+   */
+  warn (message: string, payload: Object): void {
+    console.warn(this._buildPayload(LogLevel.WARN, message, payload))
+  }
+
+  /**
+   * Creates the log structure.
+   *
+   * @param severity [LogLevel] the log level.
+   * @param message  [String] is the description of log(i.e. "Lorem ipsum dolor sit amet").
+   * @param payload [Object] is a JSON object that represents the content of the event.
+   */
+  protected _buildPayload (
+    severity: LogLevel,
+    message: string,
+    payload: Object
+  ): string {
+    const logEvent: LogMessage = {
+      application: this.application,
+      environment: this.environment,
+      level: severity,
+      message: message,
+      timestamp: new Date().toISOString()
     }
+
+    return JSON.stringify({ ...logEvent, ...payload })
   }
-
-  return JSON.stringify(data)
 }
 
-/**
- * Logs an info log.
- *
- * @param message is a JSON object that represents the body of the event.
- * @param event  is the name of request event (request_started, request_finished and etc)
- * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
- */
-// prettier-ignore
-function info (message: unknown, event: string, messageEvent: string): void {
-  console.info(createMessage(message, event, messageEvent, LogLevel.INFO))
-}
-
-/**
- * Logs a warn log.
- *
- * @param message is a JSON object that represents the body of the event.
- * @param event  is the name of request event (request_started, request_finished and etc)
- * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
- */
-// prettier-ignore
-function warn (message: unknown, event: string, messageEvent: string): void {
-  console.warn(createMessage(message, event, messageEvent, LogLevel.WARN))
-}
-
-/**
- * Logs an error log.
- *
- * @param message is a JSON object that represents the body of the event.
- * @param event  is the name of request event (request_started, request_finished and etc)
- * @param messageEvent is the name of the system event (BookingFound, BookingNotFound and etc). The default value is 'request'.
- */
-// prettier-ignore
-function error (message: unknown, event: string, messageEvent: string): void {
-  console.error(createMessage(message, event, messageEvent, LogLevel.ERROR))
-}
-
-export default { info, warn, error }
+export { Kiev }
