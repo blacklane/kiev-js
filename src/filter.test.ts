e99fde64de5c497423f3da1648b5006268dea487
Rodrigo Pinto <rpinto@M20180035.fritz.box>
2021-06-23 16:54:17 +0200

    Implementing attributes filtering class.

--- src/filter.test.ts
+++ src/filter.test.ts
@@ -1,32 +1,71 @@
 import { AttributesFilter } from './filter'
 
 let attrsFilter: AttributesFilter
+let filterConfig: string[]
 
 const logToBeFiltered = {
-  user: {
-    firstName: 'John',
-    lastName: 'Doe',
-    rolePolicy: 'admin',
-    email: 'john.doe@example.com',
-    settings: {
-      darkMode: false
-    }
+  firstName: 'John',
+  lastName: 'Doe',
+  rolePolicy: 'admin',
+  email: 'john.doe@example.com',
+  address: {
+    street: 'Some street, 99, 12345-67'
+  },
+  settings: {
+    darkMode: false,
+    clientToken: 'SfieRBC1i3ncxakwe12asdf'
   }
 }
 
 describe('attributesFilter', () => {
   beforeEach(() => {
-    attrsFilter = new AttributesFilter()
+    attrsFilter = new AttributesFilter(filterConfig)
   })
 
   describe('filter', () => {
-    it('returns the original json when not filter configured', () => {
-      expect.assertions(1)
+    describe('when filter has not attributes configured', () => {
+      it('returns the original json when not filter configured', () => {
+        expect.assertions(1)
 
-      const expectedResult = logToBeFiltered
-      const response = attrsFilter.filter(logToBeFiltered)
+        const expectedResult = logToBeFiltered
+        const response = attrsFilter.filter(logToBeFiltered)
 
-      expect(response).toStrictEqual(expectedResult)
+        expect(response).toStrictEqual(expectedResult)
+      })
+    })
+
+    describe('when filter has attributes configured', () => {
+      beforeEach(() => {
+        filterConfig = [
+          'firstName',
+          'lastName',
+          'email',
+          'street',
+          'clientToken'
+        ]
+        attrsFilter = new AttributesFilter(filterConfig)
+      })
+
+      it('returns a new json with the params filtered', () => {
+        expect.assertions(1)
+
+        const expectedResult = {
+          firstName: '[FILTERED]',
+          lastName: '[FILTERED]',
+          rolePolicy: 'admin',
+          email: '[FILTERED]',
+          address: {
+            street: '[FILTERED]'
+          },
+          settings: {
+            darkMode: false,
+            clientToken: '[FILTERED]'
+          }
+        }
+        const response = attrsFilter.filter(logToBeFiltered)
+
+        expect(response).toStrictEqual(expectedResult)
+      })
     })
   })
 })
--- src/filter.ts
+++ src/filter.ts
@@ -1,6 +1,45 @@
 class AttributesFilter {
+  filteredText: string = '[FILTERED]'
+  filters: string[]
+
+  constructor (filters: string[] = []) {
+    this.filters = filters
+  }
+
+  /**
+   * Returns a new Object with the keys which match the filters wit data filtered.
+   *
+   * @param {Object} attrs to be filtered
+   * @return {Object} a new Object with filtered data
+   */
   public filter (attrs: Object): Object {
-    return attrs
+    return this.recursiveFilter(attrs)
+  }
+
+  private recursiveFilter (attrs: Object) {
+    const map = new Map()
+
+    Object.entries(attrs).forEach(pair => {
+      const [key, value] = pair
+
+      if (this.typeOfObject(value)) {
+        map.set(key, this.recursiveFilter(value))
+      } else if (this.shouldNotFilter(key)) {
+        map.set(key, value)
+      } else {
+        map.set(key, this.filteredText)
+      }
+    })
+
+    return Object.fromEntries(map)
+  }
+
+  private typeOfObject (value: any): boolean {
+    return typeof value === 'object'
+  }
+
+  private shouldNotFilter (key: string): boolean {
+    return !this.filters.includes(key)
   }
 }
 
